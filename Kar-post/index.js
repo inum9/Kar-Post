81436ca3b0cf1fe1b2473ecc74f58952f05ce994
[inum9] <inum9@outlook.com>
2025-07-25 00:36:46 +0530

    connecting the dtabase

---
+++ Kar-post/index.js
@@ -0,0 +1,11 @@
+import { app } from "./src/app.js";
+import { connectDB } from "./src/config/db.js";
+const port= process.env.PORT
+connectDB().then(
+    ()=>{
+    app.listen(port,()=>{
+        console.log(`server is running on ${port}`);
+        
+    });
+    }
+)
--- Kar-post/src/config/db.js
+++ Kar-post/src/config/db.js
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
 import { dbName } from "../../dbName.js";
+import  dotenv from "dotenv"
+dotenv.config({
+    path:"./.env"
+});
 const connectDB= async()=>{
 try {
         const connectionInstance=     await mongoose.connect(`${process.env.MONGO_DB_URI}/${dbName}`)
